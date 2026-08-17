c1e6b280b262fdc71468b097eef439cbc0dd1bce
Js-Innov.IA <info@jsinnovia.com>
2026-08-17 15:39:11 +0200

    fix(signage): boot into schedule-aware player activity

--- player-android/app/src/main/java/ia/jsinnov/pixeliumplayer/BootReceiver.java
+++ player-android/app/src/main/java/ia/jsinnov/pixeliumplayer/BootReceiver.java
@@ -3,9 +3,8 @@ import android.content.*;
 public class BootReceiver extends BroadcastReceiver {
   @Override public void onReceive(Context context, Intent intent) {
     if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-      Intent launch = new Intent(context, MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+      Intent launch = new Intent(context, ScheduledMainActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
       context.startActivity(launch);
     }
   }
 }
-
