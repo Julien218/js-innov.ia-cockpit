8c8ee9f8014e9448ffef1517713b0b407c4ef392
Julien Pagin <julien.pagin.pv@gmail.com>
2026-09-24 19:40:32 +0200

    feat: simplify home around Elynea Jarvis presence

--- src/components/ElyneaHomePresence.jsx
+++ src/components/ElyneaHomePresence.jsx
@@ -2,13 +2,13 @@ import { useSyncExternalStore } from 'react';
 import ElyneaJarvisPresence from './ElyneaJarvisPresence';
 import { getJarvisMode, subscribeJarvisMode } from '@/lib/jarvisPresenceStore';
 
-export default function ElyneaHomePresence() {
+export default function ElyneaHomePresence({ callLabel = 'Parler à Elynea' }) {
   const mode = useSyncExternalStore(subscribeJarvisMode, getJarvisMode, () => 'idle');
   return (
     <div className="elynea-home-presence">
       <ElyneaJarvisPresence mode={mode} />
       <button type="button" className="jarvis-home-chip" onClick={() => window.dispatchEvent(new CustomEvent('elynea:open', { detail: { prompt: '' } }))}>
-        Parler à Elynea
+        {callLabel}
       </button>
     </div>
   );
